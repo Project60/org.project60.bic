fcbc2d36122219a01b22a712f8364ad487d2a71b
B. Endres <endres@systopia.de>
2018-11-13 13:45:36 +0100

    fxied German parser (#37)

--- CRM/Bic/Parser/DE.php
+++ CRM/Bic/Parser/DE.php
@@ -21,56 +21,45 @@ require_once 'CRM/Bic/Parser/Parser.php';
  */
 class CRM_Bic_Parser_DE extends CRM_Bic_Parser_Parser {
 
-  static $base_url = 'http://www.bundesbank.de';
-  static $page_url = 'http://www.bundesbank.de/Redaktion/DE/Standardartikel/Aufgaben/Unbarer_Zahlungsverkehr/bankleitzahlen_download.html';
-  static $link_regex = '#href="(?P<link>/Redaktion/DE/Downloads/Aufgaben/Unbarer_Zahlungsverkehr/Bankleitzahlen/.*.txt.*[?]__blob=publicationFile)"#';
-
+  protected static $base_url = 'https://www.bundesbank.de/de/aufgaben/unbarer-zahlungsverkehr/serviceangebot/bankleitzahlen/download---bankleitzahlen-602592';
+  protected static $page_url = 'https://www.bundesbank.de/resource/blob/602702/b6f18dadd412af2b8f47fac0cd8a8dd4/mL/blz-neu-txt-data.txt';
 
   public function update() {
     // first, download the page
-    $page = $this->downloadFile(CRM_Bic_Parser_DE::$page_url);
-    if (empty($page)) {
-      return $this->createParserOutdatedError(ts("Couldn't download basic page"));
-    }
-
-    // now, find the download link
-    $match = array();
-    if (!preg_match(CRM_Bic_Parser_DE::$link_regex, $page, $match)) {
-      return $this->createParserOutdatedError(ts("The information source at www.bundesbank.de has changed"));
-    }
-
-    // finally, download the data file
-    unset($page); // save some memory
-    $data_url = CRM_Bic_Parser_DE::$base_url.$match['link'];
-    $data = $this->downloadFile($data_url);
-
-    // convert to UTF8
-    $data = mb_convert_encoding($data, 'UTF-8', 'ISO-8859-1');
-
-    if (empty($data)) {
-      return $this->createParserOutdatedError(ts("Couldn't download basic page"));
-    }
-
-    // parse the lines and build up data structure
     $banks = array();
-    $lines = explode(PHP_EOL, $data);
-    unset($data); // save some memory
-    foreach ($lines as $line) {
-      $key = trim(substr($line, 0, 8));
-      if (isset($banks[$key])) continue;
-      $banks[$key] = array(
-        'value'       => $key,
-        'name'        => trim(mb_substr($line, 139, 11)),
-        'label'       => rtrim(mb_substr($line, 9, 58)),
-        'description' => mb_substr($line, 67, 5) . ' ' . rtrim(mb_substr($line, 72, 35))
-        );
+    $data = $this->downloadFile(CRM_Bic_Parser_DE::$page_url);
+    if (empty($data)) {
+      return $this->createParserOutdatedError(ts("Couldn't download data set"));
     }
+
+    $lines = preg_split('/\n/', $data);
+
+    foreach ($lines as $line) {
+      $bc_code = substr($line, 0, 8);
+      $bic     = trim(substr($line, 139, 11));
+      $name    = trim(substr($line, 107, 27));
+      $address = substr($line, 67, 5) . ' ' . trim(substr($line, 72, 35));
+
+      // we only want branches with BICs
+      if (empty($bic)) continue;
+
+      $name    = mb_convert_encoding($name,'UTF-8', 'ISO-8859-1');
+      $address = mb_convert_encoding($address,'UTF-8', 'ISO-8859-1');
+
+      $banks[$bc_code] = array(
+          'value'       => $bc_code,
+          'name'        => $bic,
+          'label'       => $name,
+          'description' => $address,
+      );
+    }
+
+    // some cleanup
     unset($lines);
-    
+
     // finally, update DB
     return $this->updateEntries('DE', $banks);
   }
-
   /*
    * Extracts the National Bank Identifier from an IBAN.
    */
